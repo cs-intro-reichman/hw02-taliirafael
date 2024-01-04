60fe25708d01ac7938b406f5f1f3e01a8077d6c8
taliirafael <154661297+taliirafael@users.noreply.github.com>
2024-01-05 03:15:41 +1100

    Add files via upload

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,23 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
+
+	int num = Integer.parseInt(args[0]); 
+		
+	for (int row = 0; row < num; row++) {
+			String space = ""; 
+			
+	if (row % 2 == 1) { 
+		space = " *";
 	}
+	else {
+		space = "* "; 
+	}
+	for (int l = 0; l < num; l++) { 
+		System.out.print(space);
+	}
+		System.out.println(""); 
+	}
+		
+} 
 }
